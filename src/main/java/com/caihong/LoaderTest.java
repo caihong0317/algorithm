b6ff53d0cf349c2d5681ad482dcbc1e8f9cbe7be
caihong <caihong@163.com>
2023-04-20 08:46:42 +0800

    Hello

--- src/main/java/com/caihong/LoaderTest.java
+++ src/main/java/com/caihong/LoaderTest.java
@@ -2,10 +2,12 @@ package com.caihong;
 
 public class LoaderTest {
     public static void main(String[] args) {
+        System.out.println("dev 的修改2");
         ClassLoader classLoader1 = ClassLoader.getSystemClassLoader();
         System.out.println(classLoader1);
         System.out.println(classLoader1.getParent());
         System.out.println(classLoader1.getParent().getParent());
+        System.out.println("dev 的修改3");
         try {
             Class<?> aClass1 = classLoader1.loadClass("com.caihong.TestLoad");
             ClassLoader classLoader2 = ClassLoader.getSystemClassLoader();
@@ -13,9 +15,13 @@ public class LoaderTest {
             System.out.println(classLoader1 == classLoader2);
             System.out.println(aClass1 == aClass2);
 
-            System.out.println("dev 的修改");
+            System.out.println("dev 的修改1");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public void test(){
+        System.out.println("Hello");
     }
 }
