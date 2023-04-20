17351f80218def09d6c4eaf5d218d9183ed35022
caihong0317 <62496887+caihong0317@users.noreply.github.com>
2023-04-20 08:58:12 +0800

    Update LoaderTest.java

--- src/main/java/com/caihong/LoaderTest.java
+++ src/main/java/com/caihong/LoaderTest.java
@@ -7,14 +7,12 @@ public class LoaderTest {
         System.out.println(classLoader1);
         System.out.println(classLoader1.getParent());
         System.out.println(classLoader1.getParent().getParent());
-        System.out.println("dev 的修改3");
         try {
             Class<?> aClass1 = classLoader1.loadClass("com.caihong.TestLoad");
             ClassLoader classLoader2 = ClassLoader.getSystemClassLoader();
             Class<?> aClass2 = classLoader2.loadClass("com.caihong.TestLoad");
             System.out.println(classLoader1 == classLoader2);
             System.out.println(aClass1 == aClass2);
-
             System.out.println("dev 的修改1");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
