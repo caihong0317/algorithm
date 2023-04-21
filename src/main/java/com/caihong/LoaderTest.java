629b1c0654f769e61773f8574b413036b07c8c9c
caihong0317 <62496887+caihong0317@users.noreply.github.com>
2023-04-21 20:57:32 +0800

    Update LoaderTest.java

--- src/main/java/com/caihong/LoaderTest.java
+++ src/main/java/com/caihong/LoaderTest.java
@@ -2,10 +2,6 @@ package com.caihong;
 
 public class LoaderTest {
     public static void main(String[] args) {
-        ClassLoader classLoader1 = ClassLoader.getSystemClassLoader();
-        System.out.println(classLoader1);
-        System.out.println(classLoader1.getParent());
-        System.out.println(classLoader1.getParent().getParent());
         try {
             Class<?> aClass1 = classLoader1.loadClass("com.caihong.TestLoad");
             ClassLoader classLoader2 = ClassLoader.getSystemClassLoader();
@@ -21,9 +17,5 @@ public class LoaderTest {
      public void work(){
         System.out.println("do working");
         System.out.println("do working");
-    }
-
-    public void say(){
-        System.out.println(str);
     }
 }
